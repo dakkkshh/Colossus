97f98b4b647c077e69a27645b2a39f9727095341
Daksh <daksh@gargtechsolutions.in>
2023-04-07 21:11:25 +0530

    added spaceModel

---
+++ backend/models/spaceModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const spaceSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        isWifi: {
+            type: Boolean,
+            default: false,
+            required: true,
+        },
+        isComputers: {
+            type: Boolean,
+            default: false,
+            required: true,
+        },
+        isAc: {
+            type: Boolean,
+            default: false,
+            required: true,
+        },
+        seats: {
+            type: [mongoose.Schema.Types.ObjectId],
+            ref: 'seat',
+            default: [],
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const spaceModel = mongoose.model('space', spaceSchema);
+module.exports = spaceModel;
